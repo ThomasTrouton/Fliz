e12e19412b3ee123bdcbd93d58654a1367716f85
Thomas Trouton <thomastrouton@gmail.com>
2017-11-23 21:44:36 +0000

    added a basic splash screen

---
+++ app/src/main/java/com/example/thoma/fliz/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.thoma.fliz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Intent intent = new Intent(this, MainActivity.class);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+
+
+    }
+}
